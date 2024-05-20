27b2cf3d026a65b49715615b8ed6cae7f871e609
manhhocok <trjeumanh1213@gmail.com>
2024-05-20 18:49:45 +0700

    firt commit

---
+++ src/images/icons/index.ts
@@ -0,0 +1,7 @@
+import SearchIcon from './search-icon';
+import HeartIcon from './heart';
+import NewIcon from './new-icon';
+import HotIcon from './hot-icon';
+import SaleIcon from './sale-icon';
+
+export { SearchIcon, HeartIcon, NewIcon, HotIcon, SaleIcon };
---
+++ src/images/index.ts
@@ -0,0 +1,60 @@
+import Logo from '../../public/logo.png';
+import Test from '../../public/test.jpg';
+import Test2 from '../../public/test2.jpg';
+import Test3 from '../../public/test3.png';
+import Test4 from '../../public/test4.jpg';
+import Test5 from '../../public/test5.jpg';
+import Test6 from '../../public/test6.jpg';
+import Test7 from '../../public/test7.jpg';
+import Test8 from '../../public/test8.jpg';
+import Test9 from '../../public/test9.jpg';
+import Banner1 from '../../public/banner1.jpg';
+import Banner2 from '../../public/banner2.jpg';
+import GiuongNgu from '../../public/giuong-ngu.jpg';
+import BanAn from '../../public/ban-an.jpg';
+import BanHoc from '../../public/ban-hoc.jpg';
+import BanTra from '../../public/ban-tra.jpg';
+import TuQuanAo from '../../public/tu-quan-ao.jpg';
+import TuSach from '../../public/tu-sach.jpg';
+import DoHuong from '../../public/do-huong.png';
+import DoKeo from '../../public/do-keo.jpg';
+import DoLim from '../../public/do-lim.png';
+import DoMin from '../../public/do-min.jpg';
+import DoNhua from '../../public/do-nhua.jpg';
+import DoVai from '../../public/do-vai.jpg';
+import GoiY1 from '../../public/goi-y1.jpg';
+import GoiY2 from '../../public/goi-y2.jpg';
+import GoiY3 from '../../public/goi-y3.jpg';
+import GoiY4 from '../../public/goi-y4.jpg';
+import LoadingImg from '../../public/load-img.png';
+export const images = {
+  Logo: Logo,
+  Test: Test,
+  Test2: Test2,
+  Test3: Test3,
+  Test4: Test4,
+  Test5: Test5,
+  Test6: Test6,
+  Test7: Test7,
+  Test8: Test8,
+  Test9: Test9,
+  Banner1: Banner1,
+  Banner2: Banner2,
+  GiuongNgu: GiuongNgu,
+  BanAn: BanAn,
+  BanHoc: BanHoc,
+  BanTra: BanTra,
+  TuQuanAo: TuQuanAo,
+  TuSach: TuSach,
+  DoHuong: DoHuong,
+  DoKeo: DoKeo,
+  DoLim: DoLim,
+  DoMin: DoMin,
+  DoNhua: DoNhua,
+  DoVai: DoVai,
+  GoiY1: GoiY1,
+  GoiY2: GoiY2,
+  GoiY3: GoiY3,
+  GoiY4: GoiY4,
+  LoadingImg: LoadingImg,
+};
